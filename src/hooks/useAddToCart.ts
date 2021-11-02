ee8562f67a9036770948d4fc3b4084212481dced
Zohar Zaborski <204124382@idf.il>
2021-11-02 18:04:36 +0200

    update

--- src/hooks/useAddToCart.ts
+++
@@ -1,9 +0,0 @@
-import { useAppDispatch } from './useAppDispatch';
-import { addToCart } from '../store/actions/cart';
-import { Product } from '../models/product';
-
-export const useAddToCart = (product: Product) => {
-	const dispatch = useAppDispatch();
-	const addCartProduct = () => dispatch(addToCart(product));
-	return { addCartProduct };
-};
--- src/hooks/useFetchCart.ts
+++
@@ -1,8 +0,0 @@
-import { useAppDispatch } from "./useAppDispatch";
-import { useEffect } from "react";
-import { ProductCart } from "../models/cart";
-import { setCart } from "../store/actions/cart";
-
-interface CartData {
-    getAllCart: ProductCart[];
-}
